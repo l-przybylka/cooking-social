9a1829da78b5b709bc4c3f085f73e778ac8681fd
Lukasz <lukaszprzybylka@gmail.com>
2022-10-08 18:51:00 +0100

    Adding more features

--- models/recipe.js
+++ models/recipe.js
@@ -1,23 +1,38 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const RecipeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    instructions: {
-        type: String,
-        // required: true
-    },
-    difficulty: {
-        type: String,
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-
-module.exports = mongoose.model('Recipe', RecipeSchema)
+module.exports = mongoose.model("Recipe", RecipeSchema);
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
+const recipesController = require('../controllers/recipes')
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get('/', homeController.getIndex);
 router.get("/login", authController.getLogin);
@@ -9,5 +11,6 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/profile", ensureAuth, recipesController.getProfile);
 
 module.exports = router
