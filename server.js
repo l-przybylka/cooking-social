ddf5f770b5eaf9d25022a3e3e5f02a3ad158ac9d
Lukasz <lukaszprzybylka@gmail.com>
2022-10-08 11:57:24 +0100

    test2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const homeRoutes = require('./routes/home')
 const recipesRoutes = require('./routes/recipes')
 const submissionRoutes = require('./routes/submission')
 
-//Use .env file in config folder
+//Use .env file in config folder test
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
