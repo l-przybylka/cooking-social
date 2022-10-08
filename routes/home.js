ff3dae7656bb3b870440e57265dfe69218241b5d
Lukasz <lukaszprzybylka@gmail.com>
2022-10-08 15:57:25 +0100

    Added login and sing up pages

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 
-router.get('/', homeController.getIndex)
+router.get('/', homeController.getIndex);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
