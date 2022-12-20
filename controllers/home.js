b935225289a004c2aab3875ebbab0ec322cdb6ff
Lukasz <lukaszprzybylka@gmail.com>
2022-12-20 08:42:21 +0000

    Moved quilljs to profile page's script to avoid console errors

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,13 @@
 const Recipe = require('../models/Recipe')
 
 module.exports = {
-    getIndex: (req,res) => {
-              res.render('index.ejs', { isLoggedIn: req.isAuthenticated()})
-        }}
+    getIndex: async (req, res) => {
+        // retrive the last 5 recipes
+        const recipes = await Recipe.find()
+            .populate("user")
+            .sort({ createdAt: 'desc' })
+            .limit(5)
+
+        console.log(recipes);
+        res.render('index.ejs', { recipe: recipes, isLoggedIn: req.isAuthenticated() })
+    }}
